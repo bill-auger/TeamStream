568c904ed48f591f8ce4bf3caacd5f9adc90c9d1
bill auger <mr_j@linuxmail.org>
2012-04-18 19:18:23 -0400

    'upload new resource files'

--- ninjam/winclient/resource.h
+++ ninjam/winclient/resource.h
@@ -1,109 +1,126 @@
-#define IDC_STATIC -1 // defined in afxres.h but MFC support is not required
 
-#define IDD_MAIN                        101
-#define IDD_CONFIG                      102
-#define IDR_MENU1                       102
-#define IDD_CFG_WAVEOUT                 103
-#define IDI_ICON1                       103
-#define IDD_CFG_ASIO                    104
-#define IDD_CFG_KS                      105
-#define IDD_CONNECT                     106
-#define IDD_LICENSE                     107
-#define IDD_PREFS                       108
-#define IDD_LOCALCHANNEL                110
-#define IDD_EMPTY_SCROLL                111
-#define IDD_LOCALLIST                   112
-#define IDD_REMOTECHANNEL               113
-#define IDD_EMPTY                       114
-#define IDD_ABOUT                       115
-#define IDR_ACCELERATOR1                116
-#define IDD_MIXERDLG                    117
-#define IDD_MIXERITEM                   118
-#define IDD_REMOTEUSER                  119
-#define IDC_COMBO1                      1000
-#define IDC_MASTERVOL                   1000
-#define IDC_CRECT                       1001
-#define IDC_COMBO2                      1001
-#define IDC_METROVOL                    1001
-#define IDC_COMBO3                      1002
-#define IDC_MASTERLBL                   1002
-#define IDC_MASTERVOLLBL                1002
-#define IDC_COMBO4                      1003
-#define IDC_RADIO2                      1003
-#define IDC_METROLBL                    1003
-#define IDC_METROVOLLBL                 1003
-#define IDC_COMBO5                      1004
-#define IDC_RADIO3                      1004
-#define IDC_MASTERPAN                   1004
-#define IDC_RADIO4                      1005
-#define IDC_METROPAN                    1005
-#define IDC_RADIO5                      1006
-#define IDC_MASTERVU                    1006
-#define IDC_EDIT1                       1007
-#define IDC_MASTERVULBL                 1007
-#define IDC_EDIT2                       1008
-#define IDC_DIV1                        1008
-#define IDC_EDIT3                       1009
-#define IDC_STATUS                      1009
-#define IDC_LATENCYINFO                 1010
-#define IDC_INTERVALPOS                 1010
-#define IDC_EDIT4                       1011
-#define IDC_CHATDIV                     1011
-#define IDC_MASTERPANLBL                1011
-#define IDC_DIV2                        1012
+// TODO: this file needs some tlc
 
-#define IDC_MASTERMUTE                  1013
-#define IDC_ANON                        1013
-#define IDC_SAVEWAVE                    1013
-#define IDC_BUTTON1                     1014
-#define IDC_METROMUTE                   1014
-#define IDC_DIV3                        1015
-#define IDC_METROPANLBL                 1016
-#define IDC_CHATDISP                    1017
-#define IDC_CUSTOM2                     1018
-#define IDC_CHATENT                     1018
-#define IDC_LOCALSCROLL                 1019
-#define IDC_REMOTESCROLL                1020
-#define IDC_LOCRECT                     1021
-#define IDC_REMOTERECT                  1022
-#define IDC_PASSLBL                     1022
-#define IDC_PASS                        1023
-#define IDC_USER                        1024
-#define IDC_HOST                        1025
-#define IDC_CHATOK                      1026
-#define IDC_STATUS2                     1028
-#define IDC_LICENSETEXT                 1029
-#define IDC_SAVEOGG                     1030
-#define IDC_SAVELOCAL                   1031
-#define IDC_SAVELOCALWAV                1032
-#define IDC_SAVEOGGBR                   1033
-#define IDC_SESSIONDIR                  1034
-#define IDC_BROWSE                      1035
-#define IDC_CHNOTE                      1036
-#define IDC_NAME                        1037
-#define IDC_TRANSMIT                    1038
-#define IDC_MUTE                        1039
-#define IDC_SOLO                        1040
-#define IDC_REMOVE                      1041
-#define IDC_AUDIOIN                     1042
-#define IDC_VU                          1043
-#define IDC_VULBL                       1044
-#define IDC_VOL                         1045
-#define IDC_PAN                         1046
-#define IDC_VOLLBL                      1047
-#define IDC_ADDCH                       1050
-#define IDC_PANLBL                      1050
-#define IDC_RECV                        1051
-#define IDC_USERNAME                    1053
-#define IDC_CHANNELNAME                 1054
-#define IDC_LOCGRP                      1054
-#define IDC_REMGRP                      1055
-#define IDC_CHATGRP                     1056
-#define IDC_VER                         1057
-#define IDC_LABEL                       1058
-#define IDC_LABEL2                      1060
-#define IDC_PASSREMEMBER                1060
-#define IDC_DIV                         1062
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1) // defined in afxres.h but MFC support is not required
+#endif
+
+// input section
+#define IDC_IOBTN                               0
+#define IDC_NAME                                1
+#define IDC_VOL                                 2
+#define IDC_VOLLBL                              3
+
+// output section
+#define IDC_AUDIOIN                             6
+#define IDC_PAN                                 7
+#define IDC_PANLBL                              8
+#define IDC_PANLLBL                             9
+#define IDC_PANRLBL                             10
+#define IDC_FX                                  11
+#define IDC_FXCFG                               12
+#define IDC_USERGRP                             13
+#define IDC_ANON                                14
+#define IDD_LOGIN_CANCEL                       15
+#define IDD_CFG_CHANNEL                        16
+//#define IDC_AUTO_LOGIN                         13
+
+
+#define IDD_MAIN                                101
+#define IDD_CONFIG                              102
+#define IDR_MENU1                               102
+#define IDD_CFG_WAVEOUT                         103
+#define IDI_ICON1                               103
+#define IDD_CFG_ASIO                            104
+#define IDD_CFG_KS                              105
+#define IDD_CONNECT                             106
+#define IDD_LICENSE                             107
+#define IDD_PREFS                               108
+#define IDD_LOCALCHANNEL                        110
+#define IDD_EMPTY_SCROLL                        111
+#define IDD_LOCALLIST                           112
+#define IDD_REMOTECHANNEL                       113
+#define IDD_EMPTY                               114
+#define IDD_ABOUT                               115
+#define IDR_ACCELERATOR1                        116
+#define IDD_MIXERDLG                            117
+#define IDD_MIXERITEM                           118
+#define IDD_REMOTEUSER                          119
+#define IDI_ICON2                               121
+#define IDC_COMBO1                              1000
+#define IDC_MASTERVOL                           1000
+#define IDC_COMBO2                              1001
+#define IDC_CRECT                               1001
+#define IDC_METROVOL                            1001
+#define IDC_MASTERVOLLBL                        1002
+#define IDC_METROVOLLBL                         1003
+#define IDC_MASTERPAN                           1004
+#define IDC_RADIO3                              1004
+#define IDC_METROPAN                            1005
+#define IDC_RADIO4                              1005
+#define IDC_MASTERVU                            1006
+#define IDC_RADIO5                              1006
+#define IDC_EDIT1                               1007
+#define IDC_MASTERVULBL                         1007
+#define IDC_DIV1                                1008
+#define IDC_EDIT2                               1008
+#define IDC_EDIT3                               1009
+#define IDC_STATUS                              1009
+#define IDC_INTERVALPOS                         1010
+#define IDC_LATENCYINFO                         1010
+#define IDC_MASTERPANLBL                        1011
+#define IDC_DIV2                                1012
+
+#define IDC_MASTERMUTE                          1013
+#define IDC_SAVEWAVE                            1013
+#define IDC_METROMUTE                           1014
+#define IDC_DIV3                                1015
+#define IDC_METROPANLBL                         1016
+#define IDC_CHATDISP                            1017
+#define IDC_CHATENT                             1018
+#define IDC_LOCRECT                             1021
+#define IDC_PASSLBL                             1022
+#define IDC_REMOTERECT                          1022
+#define IDC_PASS                                1023
+#define IDC_USER                                1024
+#define IDC_HOST                                1025
+#define IDC_CHATOK                              1026
+#define IDC_STATUS2                             1028
+#define IDC_LICENSETEXT                         1029
+#define IDC_SAVEOGG                             1030
+#define IDC_SAVELOCAL                           1031
+#define IDC_SAVELOCALWAV                        1032
+#define IDC_SAVEOGGBR                           1033
+#define IDC_SESSIONDIR                          1034
+#define IDC_BROWSE                              1035
+#define IDC_CHNOTE                              1036
+#define IDC_TRANSMIT                            1038
+#define IDC_MUTE                                1039
+#define IDC_SOLO                                1040
+#define IDC_REMOVE                              1041
+#define IDC_VU                                  1043
+#define IDC_VULBL                               1044
+#define IDC_ADDCH                               1050
+#define IDC_RECV                                1051
+#define IDC_USERNAME                            1052
+#define IDC_CHANNELNAME                         1053
+#define IDC_LOCGRP                              1054
+#define IDC_REMGRP                              1055
+#define IDC_CHATGRP                             1056
+#define IDC_VER                                 1057
+#define IDC_LABEL                               1058
+#define IDC_LABEL2                              1059
+#define IDC_PASSREMEMBER                        1061
+#define IDC_DIV                                 1062
+
+#define IDC_DIV0                                1063
+#define IDC_DIV4                                1064
+
+// master and metro
+#define IDC_MASTERGRP                           1500
+#define IDC_METROGRP                            1501
+#define IDC_BPILBL			                        1502
+#define IDC_BPMLBL		                          1503
 
 // chat color picker
 #define IDC_COLORTOGGLE													2000
@@ -127,27 +144,31 @@
 #define IDC_COLORDKPURPLE												2018
 #define IDC_COLORLTBLACK												2019
 
-// license checkboxes
-#define IDC_AGREE																2020
-#define IDC_AGREE_ALWAYS	            					2021
-
-// TeamStream link swap buttons
+// TeamStream link buttons
 #define IDC_LINKLBL															3000
 #define IDC_LINKUP															3001
 #define IDC_LINKDN															3002
+#define IDC_LINKSLISTVIEW												3003
+#define IDC_LINKLISTBOX													3004
+
+// license checkboxes
+#define IDC_AGREE																4000
+#define IDC_AGREE_ALWAYS	            					4001
 
 // timers
-#define IDT_AUTO_JOIN_TIMER											4000
+#define IDT_AUTO_JOIN_TIMER											5000
+#define IDT_LINKS_CHAT_TIMER										5001
 
-#define ID_FILE_CONNECT                 40001
-#define ID_FILE_DISCONNECT              40002
-#define ID_FILE_QUIT                    40003
-#define ID_HELP_ABOUTNINJAM             40004
-#define ID_OPTIONS_PREFERENCES          40005
-#define ID_OPTIONS_ALWAYSONTOP          40006
-#define ID_OPTIONS_AUDIOCONFIGURATION   40007
+// menu
+#define ID_FILE_CONNECT                         40001
+#define ID_FILE_DISCONNECT                      40002
+#define ID_FILE_QUIT                            40003
+#define ID_HELP_ABOUT                           40004
+#define ID_OPTIONS_PREFERENCES                  40005
+#define ID_OPTIONS_AUDIOCONFIGURATION           40007
 
 // TeamStream menu items
 #define ID_TEAMSTREAM_ON												41000
 #define ID_TEAMSTREAM_OFF												41001
-
+#define ID_TEAMSTREAM_LOAD											41002
+#define ID_TEAMSTREAM_SAVE											41003
